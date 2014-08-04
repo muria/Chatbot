05d00f6dd8ad0f4ec3099c5a1ccc82a48cc109cf
Martin Uria <muria@evidon.com>
2014-08-03 23:13:01 -0400

    Updated urban.rb to fetch the latest definitions
    Added additional definitions by using :urban <search word> <#>

--- lib/chatbot/urban.rb
+++ lib/chatbot/urban.rb
@@ -10,13 +10,23 @@ module Chatbot
     prefix Chatbot::PREFIX
     match /urban (.+)/
 
-    def lookup(word)
+    def lookup(word, num=1)
       url = "http://www.urbandictionary.com/define.php?term=#{CGI.escape(word)}"
-      CGI.unescape_html Nokogiri::HTML(open(url)).at("div.definition").text.gsub(/\s+/, ' ') rescue nil
+      html = open(url) 
+      el = "div.meaning"
+      doc = Nokogiri::HTML(html)
+      total = doc.search(el)
+      CGI.unescape_html doc.css(el)[num-1].text.gsub(/\s+/, ' ') rescue nil
     end
 
     def execute(m, word)
-      Util.bot_reply(m, "#{word}: #{(lookup(word) || 'No results found')}")
+      args = word.match(/(.+)(\d)$/)
+      unless args.nil?
+        n = args[2].to_i     
+        word = args[1].strip 
+      end
+      n ||= 1
+      Util.bot_reply(m, "#{word}: #{(lookup(word, n) || 'No results found')}")
     end
   end
 end
